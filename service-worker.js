dc2968bb5504272ac652c12e1d71fb3b413d6413
micouleau-rml <contact.rml@orange.fr>
2026-09-16 12:35:13 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,4 @@
-const CACHE='rml-gouttiere-v64-10'; self.addEventListener('install',e=>self.skipWaiting()); self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
+const CACHE='rml-gouttiere-v65-1';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
